59df75cc63bab0ea94492b5672f0cf8770aaf118
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-01 10:46:51 -0500

    practiced map method with two examples one including a numbers array and the other a string array

---
+++ june-first-2020-map.js
@@ -0,0 +1,20 @@
+// Number Array plus ten example.
+var numbersArray = [55, 25, 75, 85];
+var mapArray = numbersArray.map(plusTen);
+
+function plusTen(num) {
+  return num + 10;
+}
+
+console.log(mapArray);
+
+
+//String Array adding greeting example
+var stringArray = ["Jim", "Bob", "Jackie", "Jimmy"];
+var greetingArray = stringArray.map(addGreeting);
+
+function addGreeting(string) {
+  return "Hello " + string;
+}
+
+console.log(greetingArray);
