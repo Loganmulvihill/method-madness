f0d70b0bee6cdd919cd23f046581462b76882e9d
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-04 22:25:35 -0500

    completed method of the day for of example

---
+++ july-fourth-2020-for-of.js
@@ -0,0 +1,15 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Simple For Of Name Array Example
+var names = ["jason", "Jingle", "Himer", "Smitt"];
+
+for (var name of names) {
+  console.log(name);
+}
+
+//Simple Addition through For Of
+var numbers = [7, 8, 9, 10];
+
+for (var number of numbers) {
+  console.log(number + 8);
+}
