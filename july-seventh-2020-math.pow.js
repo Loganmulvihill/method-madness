a53e1e206ba6cb63260867df1874023b7c5e3dcd
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-07 15:58:15 -0500

    tested out math.pow method

---
+++ july-seventh-2020-math.pow.js
@@ -0,0 +1,8 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Math.pow example (number 3 to 2nd exponent) & (number 5 to 5thexponent)
+console.log(Math.pow(3, 2));
+console.log(Math.pow(5, 5));
+
+//Square Root of 4
+console.log(Math.pow(4, .5));
