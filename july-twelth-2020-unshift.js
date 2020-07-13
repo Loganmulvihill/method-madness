0505e2a51721466e7f6bc9aab96999cdb869a2b8
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-12 23:39:04 -0500

    tested unshift method

---
+++ july-twelth-2020-unshift.js
@@ -0,0 +1,11 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Unshift Numbers array example
+let numbers = [1, 75, 86, 102];
+numbers.unshift(2, 6);
+console.log(numbers);
+
+//Unshift string array example
+let greetings = ['Hello', "Hi", "Hey"];
+greetings.unshift('Howdy', 'Hi there');
+console.log(greetings);
