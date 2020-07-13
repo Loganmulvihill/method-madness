fe963268807243a785b0f97619ec1a4206eaadaf
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-13 13:47:26 -0500

    tested shift method with numbers and string example

---
+++ july-thirteenth-2020-shift.js
@@ -0,0 +1,11 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Shift dog array example
+let dogs = ['Husky', 'Dalmation', 'Terrier'];
+dogs.shift()
+console.log(dogs);
+
+//Shift number array example
+let numbers = [7, 8, 9];
+numbers.shift();
+console.log(numbers);
