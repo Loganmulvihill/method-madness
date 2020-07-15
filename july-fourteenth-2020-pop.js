fd6bc4b6b4dae3026947a687f4b14c411b63a9a0
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-14 20:43:17 -0500

    tested the pop method on a numbers and string array

---
+++ july-fourteenth-2020-pop.js
@@ -0,0 +1,11 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+// Using the Pop method on a numbers Array
+let numbers = [78, 99, 101, 25];
+console.log(numbers.pop());
+console.log(numbers);
+
+// Utilizing the Pop method on a string Array
+let animals = ['cheetah', 'velociraptor', 'dog'];
+console.log(animals.pop());
+console.log(animals);
