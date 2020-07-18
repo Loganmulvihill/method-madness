70c8ccb842d11b5dab04766dc4480e509ada1a58
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-18 12:44:03 -0500

    tested isFinite method with a false and true value

---
+++ july-eighteenth-isFinite.js
@@ -0,0 +1,7 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//isFinite example to test a false return value for a number that isn't finite
+console.log(isFinite(75 / 0));
+
+//isFinite example to test a true return value to test a number that is finite
+console.log(isFinite(25 / 5));
