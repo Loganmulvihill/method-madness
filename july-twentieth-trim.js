84455d324a93cbb03135fa8e9bca96431144b1e6
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-20 21:57:56 -0500

    tested simple trim method

---
+++ july-twentieth-trim.js
@@ -0,0 +1,6 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Simple Trim white space example
+const animal = '     cheetah   ';
+console.log(animal);
+console.log(animal.trim());
