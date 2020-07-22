20ae3841208652e317b1f481cb32f331f8058536
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-21 21:53:53 -0500

    tested Math.random method

---
+++ july-twenty-first.js
@@ -0,0 +1,11 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Using Math.random method within a function to console.log() random numbers
+
+const random = () => {
+  console.log(Math.random() * 100);
+}
+
+random();
+random();
+random();
