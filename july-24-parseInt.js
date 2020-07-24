a4d5045c91fa87391b5ddb83c75ad761c2200c5d
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-24 09:18:52 -0500

    tested parseInt mehod

---
+++ july-24-parseInt.js
@@ -0,0 +1,13 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Simple string test with parseInt method
+const a = '10';
+console.log(parseInt(a));
+
+//Simple string test with spaces using parseInt method
+const b = '  10  ';
+console.log(parseInt(b));
+
+//Simple string test with float on number using parseInt method
+const c = '10.5';
+console.log(parseInt(c));
