5e72a3290eb58c1f067487360c1ef863a3c1e98b
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-28 20:51:48 -0500

    tested Object.keys() method

---
+++ july-28th-object-keys.js
@@ -0,0 +1,21 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Object.keys() method example
+
+const food = {
+  pizza: "pepperoni",
+  chicken: "spicy",
+  burger: "cheese"
+}
+
+console.log(Object.keys(food));
+
+//Object.keys() method example 2
+
+const numbers = {
+  1: "one",
+  2: "two",
+  3: "three"
+}
+
+console.log(Object.keys(numbers));
