c6f19fb6590d12ba3ed733cf81f9881ff183ebff
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-29 21:06:49 -0500

    tested hasOwnProperty method

---
+++ july-29-2020-object-has-Own-Property.js
@@ -0,0 +1,17 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Simple hasOwnProperty() method call
+const object = {};
+object.pizza = "pepperoni";
+
+console.log(object.hasOwnProperty('pizza'));
+
+//Second simple example with hasOwnProperty();
+
+const secondObject = {
+  fish: "salmon",
+  chicken: "parmesian",
+  beef: "waygu"
+};
+
+console.log(secondObject.hasOwnProperty("fish"));
