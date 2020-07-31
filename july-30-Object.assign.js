38067230983b922de8b3f5e5be642174d143d2f7
loganmulvihill <mulvihill.logan@gmail.com>
2020-07-30 20:53:27 -0500

    tested Object.assign() method call

---
+++ july-30-Object.assign.js
@@ -0,0 +1,11 @@
+//All methods are tested live at https://repl.it/ using JavaScript.
+
+//Simple Object.assign() method call
+const first = { a: 1, b: 2 };
+const second = { a: 2, c: 3 };
+
+const three = Object.assign(first, second);
+
+console.log(first);
+
+console.log(three);
